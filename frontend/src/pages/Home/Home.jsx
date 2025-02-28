0abd7808741e90128927ce8f520a6c0e867a194e
Chloe Casteran <chloe.casteran@gmail.com>
2025-02-28 14:40:25 +0100

    Ajout du footer

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
     return (
       <div className="body-container">
         <Header />
+        <Footer />
       </div>
     );
   };

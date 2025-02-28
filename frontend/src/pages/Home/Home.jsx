c86f4e1f89954a263fb695161f1a490a000386f0
Chloe Casteran <chloe.casteran@gmail.com>
2025-02-28 14:32:50 +0100

    Ajout du header

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header/Header';
+
+const Home = () => {
+    return (
+      <div className="body-container">
+        <Header />
+      </div>
+    );
+  };
+  
+export default Home;
